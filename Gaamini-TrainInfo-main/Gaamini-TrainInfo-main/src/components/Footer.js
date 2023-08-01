2d92d4b2658539bed668ef2daa02ba5d84bf2b99
NithyaMohan2001 <112246758+NithyaMohan2001@users.noreply.github.com>
2023-07-31 23:11:07 -0700

    Update Footer.js

--- Gaamini-TrainInfo-main/Gaamini-TrainInfo-main/src/components/Footer.js
+++ Gaamini-TrainInfo-main/Gaamini-TrainInfo-main/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footerArea'>
-      <p>Copyright © 2022, Designed and Hosted by BrijeshYadav</p>
+      <p>Copyright © 2023, Designed and Hosted by Nithya</p>
     </div>
   )
 }
